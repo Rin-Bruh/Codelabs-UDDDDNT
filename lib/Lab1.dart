303458532ab89b868413109463d6b5665d765150
Rin-Bruh <quytt.21it@vku.udn.vn>
2024-09-12 14:00:37 +0700

    first update lab1

--- lib/Lab1.dart
+++ lib/Lab1.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           backgroundColor: Colors.blueGrey,
           appBar: AppBar(
               title: const Text("I Am Rich"),
-              backgroundColor: Colors.blueGrey[900]),
+              centerTitle: true,
+              backgroundColor: Colors.orange),
           body: const Center(
             child: Image(
+              //image: AssetImage('assets/lab1/rich.jpg'),
               image: NetworkImage(
-                  "https://images.macrumors.com/t/Ecs6wOLapXR5l9E3VmlhQfO05a8=/800x0/article/2008/08/05/195117-rich.jpg?lossy"),
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWl-u6A9F4x268uHcaUxDMhIUQ2kCM_TkDoewh5xTRWff1tC7hD_BaLiAbbXndq0K6vkU&usqp=CAU"),
             ),
           )),
     ),
